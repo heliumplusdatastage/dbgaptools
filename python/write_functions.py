f4170de42b8ff511279110e89c6561fea9140b97
alexwaldrop <alex.waldrop.jr@gmail.com>
2019-07-11 15:52:58 -0400

    Created dbgap_dd_to_json function in write_functions module
    Function takes a dbgap data dictionary as a pandas dataframe.
    Checks dataframe to ensure a set of required columns is specified.
    Refactors each variable to expand encoded values section, replaces NAs with optional missing data char (default is NoneType).
    Also filters output columns to only be those specified in list of required_cols and optional_cols

--- python/write_functions.py
+++ python/write_functions.py
@@ -1,8 +1,10 @@
 import os
 from datetime import datetime
-import json
+import pandas as pd
+import logging
+import csv
 
-import python.read_functions as rd
+import constants as const
 
 def write_dbgap(x, file="", study_name=None, phs=None,
                 DD=False, dstype="", generate_fn=False):
@@ -39,16 +41,15 @@ def write_dbgap(x, file="", study_name=None, phs=None,
 
         # Make filename
         timestamp = datetime.today().strftime('%Y%m%d')
-        file = "{0}_{1}_{2}.json".format(pt1, ftype_str, timestamp)
+        file = "{0}_{1}_{2}.txt".format(pt1, ftype_str, timestamp)
 
     # Raise error if not filename provided
     if not generate_fn and not file:
         raise IOError("Please specify filename or set generate_fn=True")
 
     filename, file_extension = os.path.splitext(file)
-    if file_extension.lower() != ".json":
-        print("Warning: Output file name recommended to have .json extension")
-
+    if file_extension.lower() != ".txt":
+        logging.warning("Warning: Output file name recommended to have .txt extension")
 
     # If there is a column name like "X__*" in a DD, assume it's an encoded values
     # colname and should be set to blank
@@ -62,13 +63,65 @@ def write_dbgap(x, file="", study_name=None, phs=None,
         x.columns = colnames
 
     # write file
-    x_dict = x.to_dict(orient="records")
-    #with open(file, "w") as fh:
-    print(json.dumps(x_dict, indent=4))
-    #utils::write.table(x, file, sep="\t",  na="",  col.names=TRUE, row.names=FALSE, quote=FALSE)
+    x.to_csv(file, sep="\t", index=False, quoting=csv.QUOTE_NONE)
+
+def dbgap_dd_to_json(dd, required_cols, optional_cols=[], missing_val_char=None):
+
+    # Check to make sure all required columns present
+    errors = False
+    for required_col in required_cols:
+        if required_col not in dd.columns:
+            # Log missing column name to stderr
+            logging.error("Required column '{0}' missing from data "
+                          "dictionary input to dbgap_dd_to_json!".format(required_col))
+            errors = True
+
+    # Raise error if any required columns are missing from data dictionary input
+    if errors:
+        raise IOError("One or more required columns is missing from the data dictionary input! "
+                      "See log above for details.")
 
 
+    # Convert pandas dataframe to list of dictionaries (one for each variable)
+    dd_dict = dd.to_dict(orient="records")
 
-result_1 = rd.read_dd_file('/Users/awaldrop/PycharmProjects/dbgaptools/python/samplexls_hasheader.xlsx')
-#result_1 = rd.read_dd_file('/Users/awaldrop/PycharmProjects/dbgaptools/python/sample_xml.data_dict.xml')
-write_dbgap(result_1, study_name="Derp", DD=False, generate_fn=True, dstype="sattr")
+    # Reformat each variable record and return final dictionary
+    return [__reformat_variable(variable, required_cols+optional_cols, missing_val_char) for variable in dd_dict]
+
+def __reformat_variable(variable_dict, required_cols, missing_val_char):
+    # Convencience function to converts variable into dictionary with encoded values unpacked and only required fields present
+
+    # Unpack encoded values into separate dictionary
+    variable_dict[const.ENCODED_VALUE_FIELD] = __unpack_encoded_vals(variable_dict)
+
+    # Set any missing values to missing value character
+    for key, val in variable_dict.items():
+        variable_dict[key] = missing_val_char if pd.isnull(val) or pd.isna(val) or val in ["", None] else val
+
+    # Filter out non-required columns
+    return { key: val for key,val in variable_dict.items() if key in required_cols }
+
+def __unpack_encoded_vals(variable_dict):
+    # Convenience function to unpacks encoded values for a variable into a single dictionary
+    enc_val_dict = {}
+
+    # Add first value from VALUES field
+    if not pd.isna(variable_dict[const.VALUES_FIELD]):
+        # Only add encoding if valid (i.e. of form 'NAME=VALUE')
+        if "=" in variable_dict[const.VALUES_FIELD]:
+            enc_val_dict[variable_dict[const.VALUES_FIELD].split("=")[0]] = variable_dict[const.VALUES_FIELD].split("=")[1]
+
+    # Check additional values (e.g X__1, X__2) in numerical order to get any remaining encodings
+    i = 1
+    var_key = "X__%s" % i
+    while var_key in variable_dict:
+        # Only add encoding if valid (i.e. of form 'NAME=VALUE')
+        if not pd.isna(variable_dict[var_key]) and "=" in variable_dict[var_key]:
+            enc_val_dict[variable_dict[var_key].split("=")[0]] = variable_dict[var_key].split("=")[1]
+
+        # Increment count to move on to next numeric value column
+        var_key = "X__%s" % i
+        i += 1
+
+    # Return encoded values as dictionary
+    return enc_val_dict if enc_val_dict else None
